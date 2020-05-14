76bc794fe6b939311737fbea833591044f743d74
Tobi1kenobi <zcbtoal@ucl.ac.uk>
2020-05-15 01:29:05 +0200

    Fixed bug with gene coordinates reading

--- scripts/format_and_map_snake.py
+++ scripts/format_and_map_snake.py
@@ -17,7 +17,6 @@ def format_genes(gene_coord_df, chr_sizes_df, out_dir, windowsize_kb, print_log_
 
 	# Gets the window size in bases
 	windowsize = windowsize_kb * 1000
-
 	# Make sure we keep only genes on autosomes or sex chromosomes 
 	# (Remove sex chromosomes later in pipeline as LDSC only uses autosomes for now)
 	gene_coord_df = gene_coord_df[gene_coord_df['CHR'].isin(chr_sizes_dict.keys())]
@@ -25,14 +24,15 @@ def format_genes(gene_coord_df, chr_sizes_df, out_dir, windowsize_kb, print_log_
 
 	gene_coord_df['START'] = np.maximum(0, gene_coord_df['START'] - windowsize)
 	gene_coord_df['END'] = np.minimum(gene_coord_df['END'] + windowsize, gene_coord_df['CHR'].map(chr_sizes_dict))
-
+	
 	# Save
 	for chrom_num in chr_sizes_dict.keys():
 		chrom_df = gene_coord_df[gene_coord_df['CHR'] == chrom_num]
 		chrom_df = chrom_df[['CHR','START','END','GENE']]
-		chrom_df.to_csv('{out_dir}/genes_plus_{window}kb.{chr}.bed'.format(out_dir = out_dir,
+		chrom_df_filename = '{out_dir}/genes_plus_{window}kb.{chr}.bed'.format(out_dir = out_dir,
 																			window = windowsize_kb,
-																			chr = chrom_num), sep='\t', index=False, header = False)
+																			chr = chrom_num)
+		chrom_df.to_csv(chrom_df_filename, sep='\t', index=False, header = False)
 
 
 ###################################### MAIN ######################################
@@ -48,13 +48,13 @@ bed_out_dir = snakemake.params['bed_out_dir']
 gene_coords = snakemake.input['gene_coords']
 chr_sizes = snakemake.input['chr_sizes']
 
-gene_coords_df = pd.read_csv(gene_coords, delim_whitespace = True, index_col=None, header=None, names=['GENE', 'CHR', 'START', 'END', 'STRAND', 'GENE_NAME'])
+gene_coord_df = pd.read_csv(gene_coords, delim_whitespace = True, index_col=None, header=None,
+ names=['GENE', 'CHR', 'START', 'END', 'STRAND', 'GENE_NAME'])
 gene_coord_df = gene_coord_df.drop(['STRAND', 'GENE_NAME'], axis=1)
 # ENSG00000186092 1       69091   70008   +       OR4F5
 # ENSG00000237683 1       134901  139379  -       AL627309.1
 # ENSG00000235249 1       367640  368634  +       OR4F29
 # ENSG00000185097 1       621059  622053  -       OR4F16
-
 chr_sizes_df = pd.read_csv(chr_sizes, delim_whitespace = True, index_col=0, header=None)
-format_genes(gene_coords_df, chr_sizes_df, bed_out_dir, windowsize_kb)
+format_genes(gene_coord_df, chr_sizes_df, bed_out_dir, windowsize_kb)
 # multi_gene_sets_to_dict_of_beds(df_multi_gene_set_human, df_gene_coords, windowsize, bed_out_dir + '/tmp', bed_out_dir, out_prefix)
